3a79829d24dee41c1f27345998b56f9d9574edf0
kychanbp <ust.johnchan@gmail.com>
2021-05-01 12:04:38 +0800

    get shop info

--- src/getTopProduct.py
+++ src/getTopProduct.py
@@ -4,39 +4,55 @@
 # direct generate from postman
 import json
 import requests
+from datetime import datetime
 import pandas as pd
-from items import search_items
+from items import search_items, search_item_monitoring
 
 import pdb
 
-def main():
+def main(search_items, page_start, page_end):
 
-    dfs = []
+    search_by = 'relevancy' #'sales'
+
+
     for search_item in search_items:
-        df = search_bar(search_item)
-        dfs.append(df)
-    
-    results = pd.concat(dfs)
-    results.to_csv('../results/first_page_results.csv')
-    return results
+        dfs_searchbar = []
+        grass_time = datetime.now()
+        for page_num in range(page_start, page_end*50, 50):
+            df_searchbar = search_bar(grass_time, search_by, search_item, page_num)
+            dfs_searchbar.append(df_searchbar)
 
-def search_bar(search_item):
+        dfs_searchbar = pd.concat(dfs_searchbar)
 
-    url = f"https://shopee.my/api/v4/search/search_items?by=sales&keyword={search_item}&limit=50&newest=0&order=desc&page_type=search&version=2"
+        shop_ids = dfs_searchbar['shopid'].unique()
+
+        dfs_shop_info = []
+        for shop_id in shop_ids:
+            df_shop_info = shop_info(grass_time,shop_id)
+            dfs_shop_info.append(df_shop_info)
+
+        dfs_shop_info = pd.concat(dfs_shop_info)
+
+        dfs_searchbar.to_csv(f'../results/{search_item}.csv', index=False)
+        dfs_shop_info.to_csv(f'../results/{search_item}_shop_info.csv', index=False)
+
+    return 
+
+def search_bar(grass_time, search_by, search_item, page_num):
+
+    url = f"https://shopee.my/api/v4/search/search_items?by={search_by}&keyword={search_item}&limit=50&newest={page_num}&order=desc&page_type=search&version=2"
 
     payload={}
     headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:87.0) Gecko/20100101 Firefox/87.0',
     'Accept': '*/*',
     'Accept-Language': 'en-US,en;q=0.5',
-    'Referer': 'https://shopee.sg/search?keyword=airpods%20pro%20case&trackingId=searchhint-1619268926-56ed35dc-a4fc-11eb-b9bc-d094668303d8',
     'X-Shopee-Language': 'en',
     'X-Requested-With': 'XMLHttpRequest',
     'X-API-SOURCE': 'pc',
     'If-None-Match-': '55b03-2ff39563c299cbdc937f8ab86ef322ab',
     'DNT': '1',
     'Connection': 'keep-alive',
-    'Cookie': '_gcl_au=1.1.1541528277.1619268921; csrftoken=BDEnCqVSn3exML2Tx3OpBmd1JXYVlaLN; REC_T_ID=53dee72f-a4fc-11eb-a268-b4969132e9d2; SPC_SI=bfftocsg4.E6fvwqs9IjIwnGK0tn99PLhrLlYmToRd; _ga_4572B3WZ33=GS1.1.1619268921.1.1.1619269033.60; _ga=GA1.1.1007393196.1619268921; SPC_F=34pe4M2981UtC6mes4Xx5V51wDJvpAQu; SPC_R_T_ID="n800gn4Iy7G27T+AriKFTAoYoobSollx1WmzujS0hmLGFh/5tzRm1+Oymy7MPYf9MAUoboi6jBr+fH4gbGZgRklK4YoA62tItp04b5x4wpg="; SPC_IA=-1; SPC_EC=-; SPC_T_ID="n800gn4Iy7G27T+AriKFTAoYoobSollx1WmzujS0hmLGFh/5tzRm1+Oymy7MPYf9MAUoboi6jBr+fH4gbGZgRklK4YoA62tItp04b5x4wpg="; SPC_R_T_IV="b3i2bXUt1wUhKzY2QncjAA=="; SPC_U=-; SPC_T_IV="b3i2bXUt1wUhKzY2QncjAA=="; welcomePkgShown=true; SPC_SI=bfftocsg4.E6fvwqs9IjIwnGK0tn99PLhrLlYmToRd',
     'TE': 'Trailers'
     }
 
@@ -44,7 +60,11 @@ def search_bar(search_item):
     response = response.text
 
     df = search_bar_resposne_parse(response)
+    df['created_on'] = datetime.now()
+    df['grass_time'] = grass_time
     df['searsch_keywords'] = search_item
+    df['search_type'] = search_by
+    df['page_num'] = (page_num/50) + 1
 
     return df
 
@@ -59,5 +79,78 @@ def search_bar_resposne_parse(res):
 
     return df
 
+def product_details(grass_time,item_id, shop_id):
+    url = f"https://shopee.com.my/api/v2/item/get?itemid={item_id}&shopid={shop_id}"
+
+    payload={}
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:88.0) Gecko/20100101 Firefox/88.0',
+    'Accept': '*/*',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'X-Shopee-Language': 'en',
+    'X-Requested-With': 'XMLHttpRequest',
+    'X-API-SOURCE': 'pc',
+    'If-None-Match-': '55b03-c6966dfc1b32ac0420c7aa87b0de1a61',
+    'Connection': 'keep-alive',
+    'Sec-GPC': '1',
+    'If-None-Match': '771fe9e4fabafcd3c06808ebe640ad56',
+    'TE': 'Trailers',
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    response = response.text
+
+    df = product_details_parse(response)
+    df['created_on'] = datetime.now()
+    df['grass_time'] = grass_time   
+
+    return df
+
+def product_details_parse(res):
+    res = json.loads(res)
+    items = res['item']
+
+    df = pd.json_normalize(items)
+
+    return df
+
+def shop_info(grass_time, shop_id):
+    url = f"https://shopee.com.my/api/v4/product/get_shop_info?shopid={shop_id}"
+
+    payload={}
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:88.0) Gecko/20100101 Firefox/88.0',
+    'Accept': '*/*',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'X-Shopee-Language': 'en',
+    'X-Requested-With': 'XMLHttpRequest',
+    'X-API-SOURCE': 'pc',
+    'If-None-Match-': '55b03-37bbce88409985e577c12c337391ce3e',
+    'Connection': 'keep-alive',
+    'Sec-GPC': '1',
+    'TE': 'Trailers',
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    response = response.text
+
+    df = shop_info_parse(response)
+    df['created_on'] = datetime.now()
+    df['grass_time'] = grass_time
+
+    return df
+
+def shop_info_parse(res):
+    res = json.loads(res)
+    data = res['data']
+
+    try:
+        df = pd.json_normalize(data)
+        return df
+    except Exception as e:
+        print(e)
+        return pd.DataFrame()
+
+
 if __name__ == "__main__":
-    main()
+    main(search_item_monitoring, 0, 100)
